fdbeaf9e28cd0539905e16040f4ad891c9b8492c
changan <changan.1314@outlook.com>
2026-04-10 20:52:43 +0800

    Scene3播放速度修正：70倍速改为70%速度(0.7x)

--- promo-video/src/scenes/Scene3-AIAnalysis.tsx
+++ promo-video/src/scenes/Scene3-AIAnalysis.tsx
@@ -44,7 +44,7 @@ export const Scene3AIAnalysis: React.FC = () => {
           showShell
           shellDepth={0.12}
           shellRadius={0.16}
-          playbackRate={70}
+          playbackRate={0.7}
         />
       </ThreeCanvas>
     </AbsoluteFill>
